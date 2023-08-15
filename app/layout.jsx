0a4bbf7f198f1dc7883a5e6c7e8ba8564d1c5613
mhb1179 <mhb1179@gmail.com>
2023-08-15 11:23:25 +0330

    strict mode

--- app/layout.jsx
+++ app/layout.jsx
@@ -10,11 +10,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="fa">
-      {/* <StrictMode> */}
+      <StrictMode>
       <AuthProvider>
         <body suppressHydrationWarning={true}>{children}</body>
       </AuthProvider>
-      {/* </StrictMode> */}
+      </StrictMode>
     </html>
   );
 }
